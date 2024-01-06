0c489af79101262e87fd5b969a250822a8b301a0
Jakub Bednarczyk <122677822+sloneczny40@users.noreply.github.com>
2024-01-06 23:27:13 +0100

    adding decode function

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-
+end = False
 
 def encrypt(text, shift):
     encrypted_text = ""
@@ -33,17 +14,55 @@ def encrypt(text, shift):
             encrypted_text += new_letter
         else:
             encrypted_text += i
-    print(index_of_letter)
-    print(f"The encoded text is: {encrypted_text}")
+    print(f"\nThe encoded text is: {encrypted_text}")
+
+def decode(text, shift):
+    encrypted_text = ""
+    list_of_words = list(text)
+    for i in list_of_words:
+        if i in alphabet:
+            index_of_letter = alphabet.index(i) - shift
+            new_letter = alphabet[index_of_letter]
+            encrypted_text += new_letter
+        else:
+            encrypted_text += i
+    print(f"\nThe decoded text is: {encrypted_text}")
+
+
+import os
+import sys
+
+
+def restart():
+    print('\n\nHere we go again...\n\n')
+
+    os.execv(sys.executable, ['python'] + sys.argv)
 
 
 
+while not end:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if direction == 'encode':
+        encrypt(text, shift)
+    elif direction == 'decode':
+        decode(text, shift)
+    else:
+        print("\nYou need to type 'encode' or 'decode'")
+        restart()
+    end_question = input("Do you want to continue? (y/n):")
+    if end_question == "n":
+        end = True
+    elif end_question == "y": 
+        restart()
 
 
-if direction == 'encode':
-    encrypt(text, shift)
+# if direction == 'encode':
+#     encrypt(text, shift)
 # elif direction == 'decode':
-    # decode(text, shift)
-# else:
-#     print("You need to type 'encode' or 'decode'\n")
-#     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+#     decode(text, shift)
+# # else:
+# #     print("You need to type 'encode' or 'decode'\n")
+# #     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
